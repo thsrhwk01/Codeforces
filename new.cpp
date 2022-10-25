ef7efe53b84166d6cfdb84680a3aca39eada554a
thsrhwk01 <99wooyoung@gmail.com>
2022-10-25 17:52:50 +0900

    Uploaded_by_CWY

---
+++ new.cpp
@@ -0,0 +1,29 @@
+//
+// Created by woo-young Choi on 2022/09/29.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void input() {
+
+}
+
+void solve() {
+
+}
+
+void output() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    input();
+    solve();
+    output();
+}
---
+++ test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by woo-young Choi on 2022/09/28.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << int('0');
+}
