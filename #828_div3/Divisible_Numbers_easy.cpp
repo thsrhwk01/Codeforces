63a628d857c73254350757d8b4329b96646c392d
thsrhwk01 <99wooyoung@gmail.com>
2022-11-10 19:19:51 +0900

    Uploaded_by_CWY

---
+++ #828_div3/Divisible_Numbers_easy.cpp
@@ -0,0 +1,51 @@
+//
+// Created by woo-young Choi on 2022/11/10.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define pii pair<int, int>
+#define vii vector<pii>
+#define ll long long
+
+#define em emplace
+#define emb emplace_back
+#define range(x) x.begin(), x.end()
+#define fastio iostream::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+int a, b, c, d;
+int x, y
+ll tmp;
+
+void input() {
+    x = 0;
+    y = 0;
+
+    cin >> a >> b >> c >> d;
+}
+
+void solve() {
+    tmp = a * b;
+
+
+}
+
+void output() {
+
+}
+
+int main() {
+    fastio
+
+    int test;
+    cin >> test;
+
+    while (test--) {
+        input();
+        solve();
+        output();
+    }
+}
